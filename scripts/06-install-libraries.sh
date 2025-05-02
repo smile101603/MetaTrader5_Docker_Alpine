def2a04b90c2c9b2d483fe248bd28054e6184e6a
smile101603 <musii67776+1@gmail.com>
2025-05-02 17:50:23 +0700

    fix reinstall pip

--- scripts/06-install-libraries.sh
+++ scripts/06-install-libraries.sh
@@ -7,6 +7,7 @@ log_message "RUNNING" "06-install-libraries.sh"
 # Install MetaTrader5 library in Windows if not installed
 log_message "INFO" "Installing MetaTrader5 library and dependencies in Windows"
 if ! is_wine_python_package_installed "MetaTrader5"; then
+	$wine_executable python -m ensurepip --upgrade & 
 	$wine_executable python -m pip install --upgrade pip & 
     $wine_executable python -m pip install --no-cache-dir -r /app/requirements.txt
 fi
